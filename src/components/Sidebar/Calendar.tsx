b94aa0c92d9ed02595caaf1e037d421656b05233
Allanazarov Nodirbek <pigeonych@gmail.com>
2023-11-02 19:36:29 +0500

    add sidebar

---
+++ src/components/Sidebar/Calendar.tsx
@@ -0,0 +1,142 @@
+import { Fragment, useEffect, useRef, useState } from "react";
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  EllipsisHorizontalIcon,
+} from "@heroicons/react/20/solid";
+import { Menu, Transition } from "@headlessui/react";
+import {
+  startOfToday,
+  format,
+  eachDayOfInterval,
+  startOfMonth,
+  endOfMonth,
+  endOfWeek,
+  isToday,
+  isSameMonth,
+  isEqual,
+  parse,
+  add,
+  getDay,
+  startOfWeek,
+} from "date-fns";
+import { translateDate } from "../utils/translateDate";
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Calendar() {
+  const today = startOfToday();
+  const [selectedDay, setSelectedDay] = useState(today);
+  const [currentMonth, setCurrentMonth] = useState(format(today, "MMMM-yyyy"));
+  let firstDayCurrentMonth = parse(currentMonth, "MMMM-yyyy", new Date());
+  const newDays = eachDayOfInterval({
+    start: startOfWeek(firstDayCurrentMonth),
+    end: endOfWeek(endOfMonth(firstDayCurrentMonth)),
+  });
+
+  const nextMonth = () => {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+    setCurrentMonth(format(firstDayNextMonth, "MMMM-yyyy"));
+  };
+
+  const prevMonth = () => {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
+    setCurrentMonth(format(firstDayNextMonth, "MMMM-yyyy"));
+  };
+
+  return (
+    <div className="flex h-fit w-full flex-col">
+      <div className="hidden w-full max-w-md flex-none md:block">
+        <div className="flex items-center text-center text-gray-900">
+          <button
+            type="button"
+            onClick={prevMonth}
+            className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+          >
+            <span className="sr-only">Previous month</span>
+            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+          <div className="flex-auto text-base font-bold">
+            {translateDate(format(firstDayCurrentMonth, "MMMM yyyy"))}
+          </div>
+          <button
+            onClick={nextMonth}
+            type="button"
+            className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+          >
+            <span className="sr-only">Next month</span>
+            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="mt-6 grid grid-cols-7 text-center text-xs leading-6 text-gray-500">
+          <div>Пн</div>
+          <div>Вт</div>
+          <div>Ср</div>
+          <div>Чт</div>
+          <div>Пт</div>
+          <div>Сб</div>
+          <div>Вс</div>
+        </div>
+        <div className="isolate mt-2 grid grid-cols-7 rounded-lg text-sm bg-white">
+          {newDays.map((day, dayIdx) => (
+            <div
+              key={day.toString()}
+              className={classNames(
+                "hover:bg-gray-100 border-gray-200 rounded-lg",
+                dayIdx === 0 && colStartClasses[getDay(day)],
+                "focus:z-10 text-center bg-white",
+              )}
+            >
+              <button
+                type="button"
+                onClick={() => setSelectedDay(day)}
+                className={classNames(
+                  (isEqual(day, selectedDay) || isToday(day)) &&
+                    "font-semibold",
+                  isEqual(day, selectedDay) && "text-white",
+                  !isEqual(day, selectedDay) &&
+                    isSameMonth(day, firstDayCurrentMonth) &&
+                    !isToday(day) &&
+                    "text-gray-900",
+                  !isEqual(day, selectedDay) &&
+                    !isSameMonth(day, firstDayCurrentMonth) &&
+                    !isToday(day) &&
+                    "text-gray-400",
+                  isToday(day) && !isEqual(day, selectedDay) && "text-brand",
+                  dayIdx === 0 && "rounded-tl-lg",
+                  dayIdx === 6 && "rounded-tr-lg",
+                  dayIdx === newDays.length - 7 && "rounded-bl-lg",
+                  dayIdx === newDays.length - 1 && "rounded-br-lg",
+                )}
+              >
+                <time
+                  dateTime={format(day, "yyyy-MM-dd")}
+                  className={classNames(
+                    "mx-auto flex h-7 w-7 items-center justify-center rounded-lg",
+                    isEqual(day, selectedDay) && isToday(day) && "bg-brand",
+                    isEqual(day, selectedDay) && !isToday(day) && "bg-brand",
+                  )}
+                >
+                  {format(day, "d")}
+                </time>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+let colStartClasses = [
+  "",
+  "col-start-1",
+  "col-start-2",
+  "col-start-3",
+  "col-start-4",
+  "col-start-5",
+  "col-start-6",
+];
---
+++ src/components/Sidebar/Navigation.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  AdjustmentsIcon,
+  ArchiveIcon,
+  CashIcon,
+  CogIcon,
+  CollectionIcon,
+  IdentificationIcon,
+  LightBulbIcon,
+  UserGroupIcon,
+} from "../assets/icons/sidebar";
+import classNames from "../utils/classNames";
+import { NavigationItem } from "../interfaces";
+
+const navigations: NavigationItem[] = [
+  { name: "Персонал", href: "#", icon: IdentificationIcon },
+  { name: "Клиенты", href: "#", icon: UserGroupIcon },
+  { name: "Услуги", href: "#", icon: AdjustmentsIcon },
+  { name: "Склад", href: "#", icon: ArchiveIcon },
+  {
+    name: "Бронирования",
+    href: "#",
+    icon: CollectionIcon,
+  },
+  { name: "Интеграции", href: "#", icon: LightBulbIcon },
+  { name: "Билинг", href: "#", icon: CashIcon },
+  { name: "Настройки", href: "#", icon: CogIcon },
+];
+const Navigation: React.FC<{}> = () => {
+  const [currentNavigation, setCurrentNavigation] = React.useState(
+    navigations[0],
+  );
+
+  return (
+    <nav className="flex flex-1 flex-col">
+      <ul role="list" className="flex flex-1 flex-col gap-y-7">
+        <li>
+          <ul role="list" className="-mx-2 space-y-1">
+            {navigations.map((item) => {
+              const isCurrent = item.name === currentNavigation.name;
+              return (
+                <li key={item.name} onClick={() => setCurrentNavigation(item)}>
+                  <a
+                    href={item.href}
+                    className={classNames(
+                      isCurrent
+                        ? "bg-gray-50 text-brand"
+                        : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                      "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold",
+                    )}
+                  >
+                    <item.icon
+                      className={classNames(
+                        isCurrent
+                          ? "text-indigo-600"
+                          : "text-gray-400 group-hover:text-indigo-600",
+                        "h-6 w-6 shrink-0",
+                      )}
+                    />
+                    {item.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
